f577ad30a01e0e5c1bf63e9004ab35ee8c7fe248
johanpramudito <ajohanpramudito@gmail.com>
2025-03-14 00:59:21 +0700

    feat: add poppins font

--- fe-plattax/src/app/layout.tsx
+++ fe-plattax/src/app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import AuthProvider from "@/components/client-layout";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
   title: {
@@ -17,9 +24,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <meta name="apple-mobile-web-app-title" content="SD-WAN Tableau Telkom" />
-      <body className={``}>
+    <html lang="en" className={poppins.variable} suppressHydrationWarning>
+      <body className={`antialiased`}>
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
