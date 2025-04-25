9ba2109e404d78568282f3d87bc688a4c6d3057a
johanpramudito <ajohanpramudito@gmail.com>
2025-04-26 00:16:14 +0700

    feat: favicon

--- fe-plattax/src/app/layout.tsx
+++ fe-plattax/src/app/layout.tsx
@@ -25,6 +25,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={poppins.variable} suppressHydrationWarning>
+      <link
+        rel="icon"
+        type="image/png"
+        href="/favicon-96x96.png"
+        sizes="96x96"
+      />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <meta name="apple-mobile-web-app-title" content="Plat-Tax" />
+      <link rel="manifest" href="/site.webmanifest" />
       <body className={`antialiased`}>
         <AuthProvider>{children}</AuthProvider>
       </body>
